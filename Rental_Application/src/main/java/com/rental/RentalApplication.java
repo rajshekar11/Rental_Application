8e4b04777565cdae787157c54c31bc5c4b52f6ac
rajshekar11 <rajshekar767@gmail.com>
2023-04-12 20:54:37 +0530

    updated

--- Rental_Application/src/main/java/com/rental/RentalApplication.java
+++ Rental_Application/src/main/java/com/rental/RentalApplication.java
@@ -2,6 +2,9 @@ package com.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -31,5 +34,15 @@ public class RentalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RentalApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 
 }
